10c2350db1b80d879cbcd119a4e62340c0dc54a1
Ricfonte <133002981+Ricfonte@users.noreply.github.com>
2023-10-15 22:54:49 -0300

    prueba

--- myproyecto/src/components/pokemon/pokemon.js
+++ myproyecto/src/components/pokemon/pokemon.js
@@ -8,6 +8,8 @@ class Pokemon extends Component{
                 <img width={200} src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/867.png"></img>
                 <br></br>
                 <button >Hazme Click</button>
+                <button >Hazme aqui</button>
+                
             </div>
         );
     }
